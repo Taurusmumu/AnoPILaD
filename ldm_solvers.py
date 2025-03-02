774dc679f2dedb56a6b263a0d76ed771861c612d
Taurusmumu <jm12456@163.com>
2025-03-02 20:39:21 +0900

    reconstruction

---
+++ ldm_solvers.py
@@ -0,0 +1,274 @@
+import torch.nn as nn
+from typing import Any, Callable, Dict, Optional
+from compel import Compel
+import torch
+import numpy as np
+import torch.nn.functional as F
+from diffusers import DDIMScheduler, StableDiffusionPipeline,  PNDMScheduler
+from tqdm import tqdm
+
+
+__SOLVER__ = {}
+
+def register_solver(name: str):
+    def wrapper(cls):
+        if __SOLVER__.get(name, None) is not None:
+            raise ValueError(f"Solver {name} already registered.")
+        __SOLVER__[name] = cls
+        return cls
+    return wrapper
+
+def get_solver(name: str, **kwargs):
+    if name not in __SOLVER__:
+        raise ValueError(f"Solver {name} does not exist.")
+    return __SOLVER__[name](**kwargs)
+
+class StableDiffusion():
+    def __init__(self,
+                 solver_config: Dict,
+                 model_key:str="runwayml/stable-diffusion-v1-5",
+                 pipeline_path="runwayml/stable-diffusion-v1-5",
+                 device: Optional[torch.device]=None,
+                 **kwargs):
+        self.device = device
+
+        self.dtype = kwargs.get("pipe_dtype", torch.float16)
+        pipe = StableDiffusionPipeline.from_pretrained(model_key, torch_dtype=self.dtype).to(device)
+        pipe.load_lora_weights(pipeline_path)
+        self.vae = pipe.vae
+        self.vae_scale_factor = 2 ** (len(self.vae.config.block_out_channels) - 1)
+        self.tokenizer = pipe.tokenizer
+        self.text_encoder = pipe.text_encoder
+        self.compel = Compel(tokenizer=pipe.tokenizer, text_encoder=pipe.text_encoder)
+        self.unet = pipe.unet
+
+        self.scheduler = PNDMScheduler.from_pretrained(model_key, subfolder="scheduler")
+        total_timesteps = len(self.scheduler.alphas)
+        self.scheduler.set_timesteps(solver_config.num_sampling, device=device)
+        self.skip = total_timesteps // solver_config.num_sampling
+
+        self.final_alpha_cumprod = self.scheduler.final_alpha_cumprod.to(device)
+        self.scheduler.alphas_cumprod = torch.cat([torch.tensor([1.0]), self.scheduler.alphas_cumprod])
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        self.sample(*args, **kwargs)
+
+    def sample(self, *args: Any, **kwargs: Any) -> Any:
+        raise NotImplementedError("Solver must implement sample() method.")
+
+    def sample_forward_backward(self, *args: Any, **kwargs: Any) -> Any:
+        raise NotImplementedError("Solver must implement sample_forward_backward() method.")
+
+    def alpha(self, t):
+        at = self.scheduler.alphas_cumprod[t] if t >= 0 else self.final_alpha_cumprod
+        return at
+
+    @torch.no_grad()
+    def get_text_embed(self, null_prompt, prompt):
+        """
+        Get text embedding.
+        args:
+            null_prompt (str): null text
+            prompt (str): guidance text
+        """
+        # null text embedding (negation)
+        null_text_input = self.tokenizer(null_prompt,
+                                         padding='max_length',
+                                         max_length=self.tokenizer.model_max_length,
+                                         return_tensors="pt",)
+        null_text_embed = self.text_encoder(null_text_input.input_ids.to(self.device))[0]
+
+        # text embedding (guidance)
+        if prompt == "":
+            text_input = self.tokenizer(prompt,
+                                        padding='max_length',
+                                        max_length=self.tokenizer.model_max_length,
+                                        return_tensors="pt",
+                                        truncation=True)
+            text_embed = self.text_encoder(text_input.input_ids.to(self.device))[0]
+        else:
+            text_embed = self.compel(prompt)
+
+        return null_text_embed, text_embed
+
+    def encode(self, x):
+        """
+        xt -> zt
+        """
+        return self.vae.encode(x).latent_dist.sample() * 0.18215
+
+    def decode(self, zt):
+        """
+        zt -> xt
+        """
+        zt = 1/0.18215 * zt
+        img = self.vae.decode(zt).sample.float()
+        return img
+
+    def predict_noise(self,
+                      zt: torch.Tensor,
+                      t: torch.Tensor,
+                      uc: torch.Tensor,
+                      c: torch.Tensor):
+        """
+        compuate epsilon_theta for null and condition
+        args:
+            zt (torch.Tensor): latent features
+            t (torch.Tensor): timestep
+            uc (torch.Tensor): null-text embedding
+            c (torch.Tensor): text embedding
+        """
+        t_in = t.unsqueeze(0)
+        if uc is None:
+            noise_c = self.unet(zt, t_in, encoder_hidden_states=c)['sample']
+            noise_uc = noise_c
+        elif c is None:
+            noise_uc = self.unet(zt, t_in, encoder_hidden_states=uc)['sample']
+            noise_c = noise_uc
+        else:
+            c_embed = torch.cat([uc, c], dim=0)
+            z_in = torch.cat([zt] * 2)
+            noise_pred = self.unet(z_in, t_in, encoder_hidden_states=c_embed)['sample']
+            noise_uc, noise_c = noise_pred.chunk(2)
+
+        return noise_uc, noise_c
+
+    def initialize_latent(self,
+                          method: str='random',
+                          src_img: Optional[torch.Tensor]=None,
+                          target_size=None,
+                          start_lambda=1000,
+                          **kwargs):
+        if method == 'ddim':
+            z = self.inversion(self.encode(src_img.to(self.dtype).to(self.device)),
+                               kwargs.get('uc'),
+                               kwargs.get('c'),
+                               cfg_guidance=kwargs.get('cfg_guidance', 0.0))
+        elif method == 'npi':
+            z = self.inversion(self.encode(src_img.to(self.dtype).to(self.device)),
+                               kwargs.get('c'),
+                               kwargs.get('c'),
+                               cfg_guidance=1.0)
+        elif method == 'random':
+            size = kwargs.get('latent_dim', (
+                1, 4, target_size[0] // self.vae_scale_factor, target_size[0] // self.vae_scale_factor))
+            z = torch.randn(size).to(self.device)
+            if src_img is not None:
+                src_latent = self.encode(src_img)
+                start_lambda = torch.randint(start_lambda, start_lambda+1, (1,), device=self.device)
+                z = self.scheduler.add_noise(src_latent, z, start_lambda)
+        else:
+            raise NotImplementedError
+
+        return z.requires_grad_()
+
+@register_solver("pndm")
+class BasePNDM(StableDiffusion):
+    """
+    Basic DDIM solver for SD.
+    Useful for text-to-image generation
+    """
+
+    @torch.autocast(device_type='cuda', dtype=torch.float16)
+    def sample_forward_backward(self,
+                                gt,
+                                cfg_guidance=7.5,
+                                prompt=["",""],
+                                target_size=(256, 256),
+                                callback_fn=None,
+                                start_lambda=1000,
+                                **kwargs):
+        """
+                Main function that defines each solver.
+                This will generate samples without considering measurements.
+                """
+        uc, c = self.get_text_embed(null_prompt=prompt[0], prompt=prompt[1])
+        # Initialize zT
+        zt = self.initialize_latent(target_size=target_size, src_img=gt, start_lambda=start_lambda)
+        zt = zt.requires_grad_()
+
+        # get the index of the start_lambda
+        neighbor_index = np.argmin(np.absolute(self.scheduler.timesteps.cpu().numpy() - start_lambda))
+        start_lambda = self.scheduler.timesteps[neighbor_index]
+        start_lambda_idx = (self.scheduler.timesteps == start_lambda).nonzero(as_tuple=True)[0].item()
+        pbar = tqdm(self.scheduler.timesteps[start_lambda_idx:], desc="SD")
+        for step, t in enumerate(pbar):
+            at = self.alpha(t)
+            at_prev = self.alpha(t - self.skip)
+            with torch.no_grad():
+                noise_uc, noise_c = self.predict_noise(zt, t, uc, c)
+                noise_pred = noise_uc + cfg_guidance * (noise_c - noise_uc)
+                # tweedie
+            z0t = (zt - (1 - at).sqrt() * noise_pred) / at.sqrt()
+            zt = at_prev.sqrt() * z0t + (1 - at_prev).sqrt() * noise_pred
+            zt.detach_()
+            if callback_fn is not None:
+                callback_kwargs = {'z0t': z0t.detach(),
+                                   'zt': zt.detach(),
+                                   'decode': self.decode}
+                callback_kwargs = callback_fn(step, t, callback_kwargs)
+                z0t = callback_kwargs["z0t"]
+                zt = callback_kwargs["zt"]
+
+        # for the last step, do not add noise
+        gt_encode = self.encode(gt.to(self.dtype).to(self.device))
+        loss = nn.MSELoss(reduction="none")(gt_encode, z0t)
+        loss = loss.mean(dim=(1, 2, 3))
+        img = self.decode(z0t)
+        img = img.clamp(-1, 1)
+
+        return img, loss
+
+
+    @torch.autocast(device_type='cuda', dtype=torch.float16)
+    def sample(self,
+               cfg_guidance=7.5,
+               prompt=["",""],
+               target_size=(512, 512),
+               callback_fn=None,
+               **kwargs):
+        """
+        Main function that defines each solver.
+        This will generate samples without considering measurements.
+        """
+
+        # Text embedding
+        uc, c = self.get_text_embed(null_prompt=prompt[0], prompt=prompt[1])
+
+        # Initialize zT
+        zt = self.initialize_latent(target_size=target_size)
+        zt = zt.requires_grad_()
+
+        # Sampling
+        pbar = tqdm(self.scheduler.timesteps, desc="SD")
+        for step, t in enumerate(pbar):
+            at = self.alpha(t)
+            at_prev = self.alpha(t - self.skip)
+
+            with torch.no_grad():
+                noise_uc, noise_c = self.predict_noise(zt, t, uc, c)
+                noise_pred = noise_uc + cfg_guidance * (noise_c - noise_uc)
+
+            # tweedie
+            z0t = (zt - (1-at).sqrt() * noise_pred) / at.sqrt()
+
+            # add noise
+            zt = at_prev.sqrt() * z0t + (1-at_prev).sqrt() * noise_pred
+
+            if callback_fn is not None:
+                callback_kwargs = {'z0t': z0t.detach(),
+                                    'zt': zt.detach(),
+                                    'decode': self.decode}
+                callback_kwargs = callback_fn(step, t, callback_kwargs)
+                z0t = callback_kwargs["z0t"]
+                zt = callback_kwargs["zt"]
+
+        # for the last step, do not add noise
+        img = self.decode(z0t)
+        img = (img / 2 + 0.5).clamp(0, 1)
+        return img.detach().cpu()
+
+
+if __name__ == "__main__":
+    # print all list of solvers
+    print(f"Possble solvers: {[x for x in __SOLVER__.keys()]}")
---
+++ rec_generate.py
@@ -0,0 +1,151 @@
+import sys
+sys.path.append('./')
+sys.path.append('../')
+sys.path.append('../../')
+
+import argparse
+from pathlib import Path
+import os
+import csv
+import torch
+from tqdm import tqdm
+from torch.utils.data import Dataset
+from munch import munchify
+from torchvision.utils import save_image
+from PIL import Image
+from ldm_solvers import get_solver
+from utils.callback_util import ComposeCallback
+from utils.log_util import create_workdir, set_seed
+import pandas as pd
+import numpy as np
+import lpips
+import json
+from accelerate import PartialState
+from torchvision import transforms
+
+
+class RecDataset(Dataset):
+    def __init__(self, csv_path, image_root_path, transformer, out_dir):
+        self.csv_path = csv_path
+        self.image_root_path = image_root_path
+        self.transformer = transformer
+        csv_file = pd.read_csv(csv_path)
+        print(f'Load for {len(list(csv_file["file_name"]))} samples')
+
+        done_fn_list = []
+        for path, dir, files in os.walk(os.path.join(out_dir, "result")):
+            for fn in files:
+                ext = os.path.splitext(fn)[-1]
+                if ext.lower() not in ('.png', '.jpg'):
+                    continue
+                file_path = os.path.join(path.split('/')[-1], fn)
+                done_fn_list.append(file_path)
+
+        done_fn_list = [f"{fn.split('.')[0][:-4]}.png" for fn in done_fn_list]
+        csv_file = csv_file.loc[~csv_file["file_name"].isin(done_fn_list)]
+        self.file_names = list(csv_file["file_name"])
+        self.texts = list(csv_file["text"])
+        print(f'Again Load for {len(list(csv_file["file_name"]))} samples')
+
+    def __len__(self):
+        return len(self.file_names)
+
+    def __getitem__(self, index):
+        fn = self.file_names[index]
+        text = self.texts[index]
+        image_path = os.path.join(self.image_root_path, fn)
+        image = Image.open(image_path).convert("RGB")
+        image = self.transformer(image)
+
+        dir = fn.split('/')[0]
+        fn = fn.split('/')[1]
+        basename = fn.split('.')[0]
+        return dir, fn, basename, image, text
+
+def main():
+    file = 'test_out.json'
+    with open(f'configs/{file}', 'r') as f:
+        f_args = json.load(f)
+
+    parser = argparse.ArgumentParser(description="Latent Diffusion")
+    parser.add_argument("--workdir", type=Path, default=f_args["workdir"])
+    parser.add_argument("--pipeline_path", type=str, default=f_args["pipeline_path"])
+    parser.add_argument("--data_path", type=str, default=f_args["data_path"])
+    parser.add_argument("--NFE", type=int, default=f_args["NFE"])
+    parser.add_argument("--start_lambda", type=int, default=f_args["start_lambda"])
+    parser.add_argument("--img_resolution", type=Path, default=f_args["img_resolution"])
+    parser.add_argument("--num_workers", type=str, default=f_args["num_workers"])
+    parser.add_argument("--batch_size", type=str, default=f_args["batch_size"])
+    parser.add_argument("--seed", type=int, default=88888888)
+    parser.add_argument("--null_prompt", type=str, default="")
+    parser.add_argument("--cfg_guidance", type=float, default=7.5)
+    parser.add_argument("--method", type=str, default="pndm")
+
+    args = parser.parse_args()
+    set_seed(args.seed)
+    distributed_state = PartialState()
+    solver_config = munchify({'num_sampling': args.NFE})
+    callback = ComposeCallback(workdir=args.workdir,
+                               frequency=1,
+                               callbacks=["draw_noisy", 'draw_tweedie'])
+
+    solver = get_solver(args.method,
+                        pipeline_path=args.pipeline_path,
+                        solver_config=solver_config,
+                        device=distributed_state.device)
+
+    csv_path = os.path.join(args.data_path, "metadata.csv")
+    fieldnames = ["file_name", "mse_latent", "lpips_pixel"]
+    output_loss_path = f"{args.workdir}/{args.start_lambda}/loss.csv"  #
+    output_rec_path = f"{args.workdir}/{args.start_lambda}/"
+    create_workdir(output_rec_path, "image_result")
+
+    if not os.path.isfile(output_loss_path):
+        with open(output_loss_path, mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            # Write the header
+            writer.writeheader()
+
+    img_transforms = transforms.Compose(
+        [
+            transforms.Resize(args.img_resolution),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5]),
+        ]
+    )
+    test_dataset = RecDataset(csv_path, args.data_path, img_transforms, output_rec_path)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size,
+                                                  num_workers=args.num_workers, shuffle=False,
+                                                  )
+    loss_lpips = lpips.LPIPS(net='vgg').to(distributed_state.device)
+    for i, (subfolder, fn, basename, src_img, prompt) in tqdm(enumerate(test_dataloader), total=len(test_dataloader)):
+        src_img = src_img.to(distributed_state.device)
+        with torch.no_grad():
+            result, loss = solver.sample_forward_backward(gt=src_img,
+                                                             prompt=[list(np.repeat(args.null_prompt, len(subfolder))),
+                                                                     list(prompt)],
+                                                             cfg_guidance=args.cfg_guidance,
+                                                             target_size=(256, 256),
+                                                             callback_fn=callback,
+                                                             start_lambda=args.start_lambda,
+                                                             gamma=1e-1)
+            lpips_pixel = loss_lpips(src_img, result).squeeze(1, 2, 3)
+            result = result / 2 + 0.5
+
+        write_blocks = []
+        for j in range(len(subfolder)):
+            os.makedirs(os.path.join(output_rec_path, 'image_result', subfolder[j]), exist_ok=True)
+            save_image(result[j], f'{output_rec_path}/image_result/{subfolder[j]}/{basename[j]}_rec.png', normalize=True)
+            write_blocks.append({
+                "file_name": fn[j],
+                "mse_latent": str(loss[j].item()),
+                "lpips_pixel": str(lpips_pixel[j].item()),
+            })
+        with open(output_loss_path, mode='a', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writerows(write_blocks)
+
+
+if __name__ == "__main__":
+
+    main()
